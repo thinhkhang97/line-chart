cffcfa4a87db06a1b40a4f83fdc99856cb89e3d5
khangnguyen <thinhkhang97@gmail.com>
2022-12-10 14:57:17 +0700

    Implement frames and lines

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Dimensions, SafeAreaView, StyleSheet, View} from 'react-native';
-import Svg, {Rect} from 'react-native-svg';
+import Svg, {G, Line, Rect, Text} from 'react-native-svg';
+import {mockData1, mockData2} from './data';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const PADDING_HORIZONTAL = 24;
@@ -12,31 +13,59 @@ const Y_AXIS_WIDTH = 40;
 const CHART_AREA_WIDHT = CHART_WIDTH - Y_AXIS_WIDTH;
 const NUMBER_OF_FRAME = 5;
 
+const percentData1 = mockData1.map(value => value / mockData1[0] - 1);
+const percentData2 = mockData2.map(value => value / mockData2[0] - 1);
+
+const colors = {
+  mono40: '#1A1A1A66',
+  gray4: '#ECECEC',
+};
+
 const App: React.FC = () => {
+  const maxPercent = Math.max(
+    Math.max(...percentData1),
+    Math.max(...percentData2),
+  );
+
+  const minPercent = Math.min(
+    Math.min(...percentData1),
+    Math.min(...percentData2),
+  );
+
+  const dValue = (maxPercent - minPercent) / (NUMBER_OF_FRAME - 1);
+  const dY = CHART_AREA_HEIGHT / NUMBER_OF_FRAME;
+  const frameLinePercents = Array.from({length: NUMBER_OF_FRAME}).map(
+    (_, index) => minPercent + dValue * index,
+  );
+
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={styles.container}>
         <Svg width={CHART_WIDTH} height={CHART_HEIGHT}>
-          <Rect
-            x={0}
-            y={0}
-            width={Y_AXIS_WIDTH}
-            height={CHART_HEIGHT - TIME_AREA_HEIGHT}
-            fill="blue"
-          />
-          <Rect
-            x={Y_AXIS_WIDTH}
-            y={0}
-            width={CHART_AREA_WIDHT}
-            height={CHART_AREA_HEIGHT}
-            fill="red"
-          />
+          {frameLinePercents.map((value, index) => (
+            <G key={`line_index_${index}`}>
+              <Text
+                textAnchor="end"
+                fontSize={12}
+                fill={colors.mono40}
+                x={Y_AXIS_WIDTH - 8}
+                y={CHART_AREA_HEIGHT - dY * index}>
+                {`${(value * 100).toFixed(0)}%`}
+              </Text>
+              <Line
+                x1={Y_AXIS_WIDTH}
+                x2={CHART_WIDTH}
+                y1={CHART_AREA_HEIGHT - dY * index}
+                y2={CHART_AREA_HEIGHT - dY * index}
+                stroke={colors.gray4}
+              />
+            </G>
+          ))}
           <Rect
             x={0}
             y={CHART_HEIGHT - TIME_AREA_HEIGHT}
             width={CHART_WIDTH}
             height={TIME_AREA_HEIGHT}
-            fill="green"
           />
         </Svg>
       </View>
